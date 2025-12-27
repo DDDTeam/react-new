21af4513b5f3c0af29ac2ca373a9a97ef8ee4cc0
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-12-27 16:55:25 +0300

    fix: debug

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -1,9 +1,13 @@
 import type {Component} from '../component';
 
 export const isProvider = (component: Component) => {
+  console.log("isProvider")
+  console.log("component.constructor.name", component.constructor.name)
   return component.constructor.name.includes('Provider');
 };
 
 export const isConsumer = (component: Component) => {
+  console.log("isConsumer")
+  console.log("component.constructor.name", component.constructor.name)
   return component.constructor.name.includes('Consumer');
 };
